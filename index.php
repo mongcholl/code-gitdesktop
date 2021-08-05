1404b1d02b2b8e0413099abaa76bab7ef217ac87
mongcholl <Net_1258@hotmail.com>
2021-08-05 14:38:45 +0700

    Revert "add some text"
    This reverts commit e1bbd9fd6c212dbda7c349174087063cef4db2db.

--- index.php
+++ index.php
@@ -7,9 +7,5 @@
 </head>
 <body>
     <h1>learn git desktop</h1>
-    <p>Lorem ipsum dolor,
-     sit amet consectetur adipisicing elit. Repudiandae culpa amet 
-     sequi, nam ea voluptate. Dicta, delectus ea. Molestias, nostrum magni 
-     nulla consequuntur voluptate obcaecati explicabo fuga sunt atque totam.</p>
 </body>
 </html>
